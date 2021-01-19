20b746f68362c11b105fc1a3a804486c97a840f2
McGravel <aclay@momentum-mod.org>
2021-01-19 04:52:29 +0000

    Initial Commit Initial user input, date, and time stuff

---
+++ Mood.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MoodLog
+{
+    public class Mood
+    {
+        public string CurrentDate   { get; }
+        public string CurrentTime   { get; }
+        public string CurrentRating { get; }
+
+        public Mood()
+        {
+            ConsoleKeyInfo key = default;
+            while (!IsNumber(key.Key))
+            {
+                key = Console.ReadKey();
+            }
+
+            CurrentDate = DateTime.Now.ToShortDateString();
+            CurrentTime = DateTime.Now.ToShortTimeString();
+            CurrentRating = key.KeyChar.ToString();
+        }
+        
+        private static bool IsNumber(ConsoleKey inputKey)
+        {
+            return ((inputKey >= ConsoleKey.D0 && inputKey <= ConsoleKey.D9) ||
+                    (inputKey >= ConsoleKey.NumPad0 && inputKey <= ConsoleKey.NumPad9));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MoodLog
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Current Date for entries: {DateTime.Now.Date.ToShortDateString()}");
+            Console.WriteLine("What's your mood, in a rating from 0(Lowest) to 9(Highest)?");
+            var mood = new Mood();
+            Console.WriteLine($"\n{mood.CurrentDate} @ {mood.CurrentTime}: {mood.CurrentRating}");
+        }
+    }
+}
