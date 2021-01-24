f88da996567cd5d552adb46aebc44c415928d1de
McGravel <aclay@momentum-mod.org>
2021-01-24 17:15:10 +0000

    Make filename month and year specific Helps to separate things out a bit more cleanly rather than outputting to one monolithic file

--- Program.cs
+++ Program.cs
@@ -9,8 +9,7 @@ namespace MoodLog
     {
         private static void Main(string[] args)
         {
-            // TODO: Split these files into monthly files perhaps? i.e. "02_2021.json" or something.
-            var fileName = AppDomain.CurrentDomain.BaseDirectory + @"\mood.json";
+            var fileName = AppDomain.CurrentDomain.BaseDirectory +  DateTime.Today.ToString("MMM") + "_" + DateTime.Today.Year + @".json";
             
             if (!File.Exists(fileName))
             {
